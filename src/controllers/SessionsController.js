b15ad107ef5feb74ac121ba496c6659110ec2f2d
Jair Torezone <jairtorezone@gmail.com>
2024-03-04 20:47:21 -0300

    SessionsController - validating password and email

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,26 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
+
+class SessionsController {
+  async create(request, response) {
+    const { email, password } = request.body;
+
+    const user = await knex("users").where({ email }).first();
+    console.log(user);
+
+    if (!user) {
+      throw new AppError("E-mail e/ou Senha incorreto.", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("E-mail e/ou Senha incorreto.", 401);
+    }
+
+    return response.json({ user });
+  }
+}
+
+module.exports = SessionsController;
