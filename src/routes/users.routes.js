0b07a70e9afccf3960628e63db58ed488c5f7bc2
Jair Torezone <jairtorezone@gmail.com>
2024-03-06 21:42:35 -0300

    saving image to database

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,10 +4,12 @@ const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const UserAvatarController = require("../controllers/UserAvatarController");
 
 const usersRoutes = Router();
-const usersController = new UsersController();
 const upload = multer(uploadConfig.MULTER);
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
@@ -19,10 +21,7 @@ usersRoutes.patch(
   "/avatar",
   ensureAuthenticated,
   upload.single("avatar"),
-  (request, response) => {
-    console.log(request.file.filename);
-    response.json();
-  }
+  userAvatarController.update
 );
 
 module.exports = usersRoutes;
